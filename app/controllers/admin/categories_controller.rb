a19114e66abe881a85ffbeab54c60ae6f4df8854
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-11-14 00:32:36 +0000

    updated controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,24 @@
 class Admin::CategoriesController < ApplicationController
+#admin login
+http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
+
+def index
+
+  @categories= Category.order(id: :asc).all
+end
+
+def new
+  @category = Category.new
+end
+
+def create
+  @category = Category.new(category_params)
+
+  if @category.save
+    redirect_to [:admin, :categories], notice: 'Category created!'
+  else
+    render :new
+  end
+end
+
 end
