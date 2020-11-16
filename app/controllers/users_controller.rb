4d5a8d8c54a7731cc623b2ce4b0fa1049a40b096
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-11-16 22:39:24 +0000

    added comment to controller and added form logic to new registration page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # renders sign up
   def new
   end
-# receving for and creating a new users within parameters
+# receving for and creating a new users within parametersg
   def create
   end
 
