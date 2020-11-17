eef3065df1960c43d029405cac633a7e03ed579e
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-11-17 22:07:04 +0000

    added quantity check to product_spec test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -46,6 +46,16 @@ RSpec.describe Product, type: :model do
       expect(@product.errors.full_messages).to include ("Price can't be blank")
     end
 
+    it 'should contain a quantity' do
+      @category = Category.new(name: "shoes")
+      @product = Product.new(id: 200, 
+        name: "Red shoe", 
+        price_cents: 2000, 
+        quantity: nil, 
+        category: @category)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to eq ["Quantity can't be blank"]
+    end
   
       
   end
