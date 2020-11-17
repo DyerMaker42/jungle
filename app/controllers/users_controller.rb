465136f01690ab382022753d0fda86bb14bd91db
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-11-17 00:59:34 +0000

    added logic to  user controller to match form in new"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,15 @@ class UsersController < ApplicationController
   end
 # receving for and creating a new users within parametersg
   def create
+    user = User.new(user_params)
+    if user.save
+      sesion[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
   end
-
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
