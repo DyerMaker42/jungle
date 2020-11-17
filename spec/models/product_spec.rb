c3804ca12e284a8aa9a44faee1f44799699c0768
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-11-17 21:19:48 +0000

    added product new test to product_spec.rb

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it 'should create products' do
+      expect(Product.new).to be_a Product
+    end
+  end
+  
 end
