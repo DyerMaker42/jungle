967dd7b54ba989742518c9a4611a5578e5fc21af
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-11-18 03:13:00 +0000

    quite a bit of modifications troubleshooting a test error, turns out to be a migration and test issue, rolling the db and then performing rake db:test:prepare resolved the issue so now I can write the rest of the tests to pass, yes I know this is a long commit, but a lot happened and I prefer to be atomic if at all possible

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,15 @@ class UsersController < ApplicationController
   end
 # receving for and creating a new users within parametersg
   def create
-    user = User.create(user_params)
+    user = User.new(user_params)
     if user.save
-      sesion[:user_id] = user.id
+      session[:user_id] = user.id
       redirect_to '/'
     else
       redirect_to '/signup'
     end
   end
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
 
-  it 'should create a user' do
-    expect(User.create).to be_a User
+  describe 'Validations' do
+
+    # it 'should create users' do
+    #   expect(User.new).to be_a User
+    # end
+
+    it 'is valid with valid attributes' do
+      @user = User.create(
+        first_name: "testy", 
+        last_name: "test",
+        email: "test1@test.com", 
+        password: "password", 
+        password_confirmation: "password")
+      expect(@user).to be_valid
+    end
+
   end
 end
